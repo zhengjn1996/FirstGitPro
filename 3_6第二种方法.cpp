3515ff1193c57e1101c4480bd8f89e65528d652f
zhengjn1996 <31682205+zhengjn1996@users.noreply.github.com>
2017-10-15 17:40:21 +0800

    Rename 3_6.cpp to 3_6第二种方法.cpp

--- 3_6第二种方法.cpp
+++ 3_6第二种方法.cpp
@@ -2,17 +2,17 @@
 void main()
 {
    double r,k,s,c;
-   cout<<"输入半径r与整型数k=1或2或3"<<endl;
+   cout<<"娓错潤瓿曥摵r瀹呮啇璎归憭k=1靸�2靸�3"<<endl;
    cin>>r>>k;
    if(k==1)   
    {s=3.14*r*r;
-   cout<<"面积s="<<s<<endl;}
+   cout<<"於╈儩s="<<s<<endl;}
    else 
 	   if(k==2)
 	   { c=2*3.14*r;
-	   cout<<"周长c="<<c<<endl;}
+	   cout<<"榉弘個c="<<c<<endl;}
       else 
 		  if(k==3)
 		  { c=2*3.14*r,s=3.14*r*r;
-		  cout<<"面积s="<<s<<"周长c="<<c<<endl;}
+		  cout<<"於╈儩s="<<s<<"榉弘個c="<<c<<endl;}
 }
